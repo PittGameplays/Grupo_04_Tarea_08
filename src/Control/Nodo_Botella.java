850bfa41fa50a0395b095af46b97c88d50523259
PittGameplays <116464867+PittGameplays@users.noreply.github.com>
2024-05-15 18:38:48 -0500

    Actualizar Nodo_Botella.java

--- src/Control/Nodo_Botella.java
+++ src/Control/Nodo_Botella.java
@@ -5,4 +5,20 @@ import Modelo.Botella;
 public class Nodo_Botella {
     private Botella elemento;
     private Nodo_Botella siguiente;
+
+    public Botella getElemento(){
+        return elemento;
+    }
+
+    public Nodo_Botella getSiguiente(){
+        return siguiente;
+    }
+
+    public void setElemento(Botella elemento){
+        this.elemento = elemento;
+    }
+
+    public void setSiguiente(Nodo_Botella siguiente){
+        this.siguiente = siguiente;
+    }
 }
