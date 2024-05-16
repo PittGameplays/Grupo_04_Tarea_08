e0f9e50b0c6b66427f51938b327e6e585f8474f1
PittGameplays <pedroyleo05@gmail.com>
2024-05-16 16:29:36 -0500

    Grupo 6 tarea 04 ejercicio 01

--- src/Control/Lista_Cola.java
+++ src/Control/Lista_Cola.java
@@ -1,5 +1,36 @@
 package Control;
 
+import Modelo.Cola;
+
 public class Lista_Cola {
+    Nodo_Cola inicio ,fin;
+    int tamaño=0;
     
+    public boolean ColaVacia(){
+        return inicio ==null;
+    }
+ 
+    public void Agregar(Cola elemento){
+      if(tamaño<=12){
+            Nodo_Cola nuevo  = new Nodo_Cola(elemento);
+        if(ColaVacia()){
+            inicio= nuevo;
+        }else{
+            fin.siguiente =nuevo;
+        }
+        fin = nuevo;
+        tamaño++;
+      }
+    }
+    
+    public Cola EliminarCola(){
+        Cola auxiliar = inicio.elemento;
+        inicio = inicio.siguiente;
+        tamaño--;
+        return auxiliar;
+    }
+    
+    public Cola InicioCola(){
+        return inicio.elemento;
+    }
 }
--- src/Control/Nodo_Cola.java
+++ src/Control/Nodo_Cola.java
@@ -1,5 +1,35 @@
 package Control;
 
+import Modelo.Cola;
+
 public class Nodo_Cola {
+    Cola elemento;      
+    Nodo_Cola siguiente;
     
+    public Nodo_Cola(Cola elemento) {
+        this.elemento = elemento;
+        siguiente = null;
+    }
+
+    public Nodo_Cola(Cola elemento, Nodo_Cola siguiente) {
+        this.elemento = elemento;
+        this.siguiente = siguiente;
+    }
+
+    public Cola getElemento() {
+        return elemento;
+    }
+
+    public Nodo_Cola getSiguiente() {
+        return siguiente;
+    }
+
+    public void setElemento(Cola elemento) {
+        this.elemento = elemento;
+    }
+
+    public void setSiguiente(Nodo_Cola siguiente) {
+        this.siguiente = siguiente;
+    }
 }
+
